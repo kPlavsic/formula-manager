6aea59c60dcde9364941f9ab47d0057eaae5d64c
kPlavsic <kplavsic4@gmail.com>
2026-05-18 14:30:03 +0200

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/formula/manager/FormulaManagerApplication.java
@@ -0,0 +1,13 @@
+package com.formula.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormulaManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormulaManagerApplication.class, args);
+	}
+
+}
